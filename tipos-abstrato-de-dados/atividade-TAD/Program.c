f38170330f895b034244d2a7cf2ebab9d9e3539e
RUAN CARLOS DE SOUZA AZEVEDO <a2254131@debian.dacom>
2022-04-14 13:20:31 -0300

    implementando funcao substituir e funcoes auxliares e concluindo a funcao anexar

--- tipos-abstrato-de-dados/atividade-TAD/Program.c
+++ tipos-abstrato-de-dados/atividade-TAD/Program.c
@@ -7,6 +7,7 @@ int main()
 
     vet_anexar(v, 25);
     vet_anexar(v, 40);
+    vet_anexar(v, 7);
     vet_imprimir(v);
 
     return 0;
--- tipos-abstrato-de-dados/atividade-TAD/tad_vetor.c
+++ tipos-abstrato-de-dados/atividade-TAD/tad_vetor.c
@@ -3,17 +3,32 @@
 /********************************************
  * Especificação dos dados
  *******************************************/
-struct vetor
-{
-    int *vet;
-    int tam;
-    int qtd;
-};
+ struct vetor
+ {
+     int *vet;
+     int tam;
+     int qtd;
+ };
+
+/********************************************
+ * funções auxiliares
+ *******************************************/
+ int espacoLivre(Vetor* v)
+ {
+     return v->tam - v->qtd;
+ }
+
+ void definirQtd(int* qtd, int elemento)
+ {
+     if (elemento != 0)
+        *qtd += 1;
+ }
+
 
 /********************************************
  * Implementação das funções
  *******************************************/
-Vetor* vet_criar()
+ Vetor* vet_criar()
 {
     Vetor *v = (Vetor*) malloc(sizeof(Vetor));
     v->vet = (int*) calloc(1, sizeof(int));
@@ -25,11 +40,36 @@ Vetor* vet_criar()
 
 bool vet_anexar(Vetor* v, int elemento)
 {
-    int livre = v->tam - v->qtd;
-
-    if (livre > 0)
+    if (espacoLivre(v) <= 0)
     {
-        v->vet[v->tam - 1] = elemento;
+        int* vetDobrado = (int*) calloc(v->tam * 2, sizeof(int));
+        
+        if (vetDobrado == NULL)
+            return false;
+
+        // copia elemento de um vetor para outro
+        for (int i=0; i<v->tam; i++)
+        {
+            vetDobrado[i] = v->vet[i];
+        }
+
+        // desaloca vetor antigo
+        free(v->vet);
+        v->vet = vetDobrado;
+        v->tam *= 2;
+    }
+
+    v->vet[v->tam - 1] = elemento;
+    definirQtd(&(v->qtd), elemento);
+
+    return true;
+}
+
+bool vet_substituir(Vetor *v, int posicao, int novoElemento)
+{
+    if (v->tam > posicao)
+    {
+        v->vet[posicao] = novoElemento;
         return true;
     }
 
@@ -38,6 +78,8 @@ bool vet_anexar(Vetor* v, int elemento)
 
 void vet_imprimir(Vetor *v)
 {
+    printf("tam: %d\n", v->tam);
+    printf("qtd: %d\n", v->qtd);
     printf("[");
     for (int i=0; i<v->tam; i++)
     {
